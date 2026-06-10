553747f1a37692dfe37226f268022b11aa087990
Alexey Novikov <v@2dubs.com>
2026-06-10 16:07:02 -0400

    Add watchOS and visionOS support
    Add test configurations and platform declarations for
    watchOS and visionOS. Also include tvOS in the package
    platforms list.

--- Package.swift
+++ Package.swift
@@ -4,7 +4,9 @@ import PackageDescription
 
 let package = Package(
     name: "fp-swift",
-    platforms: [.macOS(.v10_15), .iOS(.v13)],
+    platforms: [
+        .macOS(.v10_15), .iOS(.v13), .tvOS(.v13), .watchOS(.v6), .visionOS(.v1),
+    ],
     products: [
         .library(
             name: "FP",
